278eb9448522464914d68b8c7394a58a17f9395e
adam <advincze@gmail.com>
2014-03-22 21:43:39 +0100

    trending articles

--- go/src/mhd2014/ipool_test.go
+++ go/src/mhd2014/ipool_test.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"log"
 	"testing"
 	"time"
 )
 
 func TestIPoolQuery(t *testing.T) {
 
-	t0 := time.Date(2014, 3, 21, 0, 0, 0, 0, time.UTC)
+	t0 := time.Now().Add(-2 * time.Hour * 24) //time.Date(2014, 3, 21, 0, 0, 0, 0, time.UTC)
 	SearchIPoolArticles(t0, t0.Add(5*time.Hour*24), []string{"www.welt.de", "www.abendblatt.de"})
+}
+
+func TestTrendingArticles(t *testing.T) {
+
+	trendingArticles := GetTrendingArticles()
+	log.Printf("trending: %v\n", len(trendingArticles))
 }
