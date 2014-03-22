1d1809036b49ad57257bb12c3341e71b3731446c
chrtze <chrtze@gmail.com>
2014-03-22 15:52:41 +0100

    added scripts

--- public/app/scripts/main.js
+++ public/app/scripts/main.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
     var questions = new Questions();
     questions.init();
     questions.activate();
+    initEvents();
 });
+
+function initEvents() {
+
+}
---
+++ public/app/scripts/questions.js
@@ -0,0 +1,16 @@
+function Questions() {
+    this.init = function() {
+        $('.question-wrapper').each(function() {
+            //TODO: initialize each question 
+        })
+    },
+    this.activate = function() {
+        //takes the active question and activates it
+        $('.question-wrapper.active .answer').each(function() {
+            $(this).css('background-image', "url(" + $(this).attr('data-img') + ")");
+        })
+    },
+    this.next = function() {
+        //TODO: swipes to the next question
+    }
+}
